55cb675a1df54a8b8c81eb8c110895a152232ec3
MrWillian <willian.dev10@gmail.com>
2022-07-01 21:16:22 -0300

    style: styled-components PaceItem

---
+++ src/components/PlaceItem/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    background-color: '#eeeeee',
+    border-radius: 10px,
+`;
+
+export const Name = styled.Text`
+    font-color: black;
+    font-size: 18px;
+`;
